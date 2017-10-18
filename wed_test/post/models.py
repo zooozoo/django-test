bfd61a69253ee1fd0c7a48d28a7d8771246715f2
zooozoo <shz0309@gmail.com>
2017-10-18 11:08:52 +0900

    post app 추가 Post model 생성

--- wed_test/post/models.py
+++ wed_test/post/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(max)
+
+    def __str__(self):
+        return self.title
